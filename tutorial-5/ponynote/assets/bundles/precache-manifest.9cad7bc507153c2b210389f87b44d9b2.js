f998d1d3aacb5d35ff41f96ed4fae50d553289d6
DmitriyPronkin <d_pronkin@master2010.com>
2019-09-23 11:23:38 +0300

    tutorial-5 1-st part complete

---
+++ tutorial-5/ponynote/assets/bundles/precache-manifest.9cad7bc507153c2b210389f87b44d9b2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b206aa7ac54012b98495",
+    "url": "/static/bundles/css/main.1a8d6906.css"
+  },
+  {
+    "revision": "1c300245aa9240dab951791855ab1d92",
+    "url": "/static/bundles/index.html"
+  },
+  {
+    "revision": "b206aa7ac54012b98495",
+    "url": "/static/bundles/js/main.b206aa7a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/bundles/media/logo.25bf045c.svg"
+  }
+]);
